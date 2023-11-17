48603b0597dc5bdf882cb6c05d8b8f32d67c7b76
Juned Raghib <zaidragib1@gmiail.com>
2023-11-17 20:36:45 +0530

    initialise the project

---
+++ src/main/java/com/example/blogappproject/BlogAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogAppProjectApplication.class, args);
+    }
+
+}
